73ae89dd9849326e35b3567804eaa13e6b97a2dc
Celso Pinheiro <cjpinheiro2@outlook.com>
2022-07-20 14:19:38 -0300

    Arquitetura do projeto, camada domain.

---
+++ ProductCatalogMvc.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductCatalogMvc.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
