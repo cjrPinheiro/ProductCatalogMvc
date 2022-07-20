2e42fca256795d2b57b360802056057f133bceea
Celso Pinheiro <cjpinheiro2@outlook.com>
2022-07-20 15:05:07 -0300

    Unit tests; Repositories Interfaces.

---
+++ ProductCatalogMvc.Domain.Tests/CategoryUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using ProductCatalogMvc.Domain.Entities;
+using ProductCatalogMvc.Domain.Validation;
+
+namespace ProductCatalogMvc.Domain.Tests
+{
+    public class CategoryUnitTest
+    {
+        [Fact(DisplayName = "Create category with valid state")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create category with invalid id")]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-90, "Category Name");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid Id value");
+        }
+
+        [Fact(DisplayName = "Create category with short name")]
+        public void CreateCategory_ShortName_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact(DisplayName = "Create category with missing name")]
+        public void CreateCategory_MissingName_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name.Name is required");
+        }
+
+        [Fact(DisplayName = "Create category with null name")]
+        public void CreateCategory_NullName_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, null);
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name.Name is required");
+        }
+    }
+}
---
+++ ProductCatalogMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProductCatalogMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalogMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> GetProductCategoryAsync(int id);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
